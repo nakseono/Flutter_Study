1444b15277af224384db045e98a3a4caf2fe07d8
NakSeoNo <nakseono@gmail.com>
2020-11-03 11:19:56 +0900

    add Drawer Icon

--- lib/main.dart
+++ lib/main.dart
@@ -7,119 +7,104 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Maple',
-      home: Grade(),
+      title: 'Appbar',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: MyPage(),
     );
   }
 }
 
-class Grade extends StatelessWidget {
+class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightGreen, // Grade #1
       appBar: AppBar(
-        // Grade #2
-        title: Text('Maple'),
-        backgroundColor: Colors.brown[800],
+        title: Text('Appbar icon menu'),
         centerTitle: true,
-        elevation: 0.0,
+        elevation: 0,
+        // leading: IconButton( //! AppBar에서 왼쪽부터 시작하는 아이콘을 만드는 속성
+        //   icon: Icon(Icons.menu),
+        //   onPressed: () {
+        //     print('menu button is clicked');
+        //   },
+        // ),
+        actions: [
+          //! AppBar에서 오른쪽부터 시작하는 아이콘을 만드는 속성
+          IconButton(
+            //! IconButton 으로 앱 아이콘을 만든다
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              //! 아이콘이 눌리면 특정 액션을 취한다 => 함수의 형태 (){}
+              print('shopping cart button is clicked');
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('search button is clicked');
+            },
+          )
+        ],
       ),
-      body: Padding(
-        // Grade #3
-        padding: EdgeInsets.fromLTRB(30.0, 40.0, 0, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+      drawer: Drawer(
+        //! 왼쪽부터 나오는 속성탭. 해당 속성을 사용하면 자연스럽게 햄버거 버튼이 생긴다
+        child: ListView(
+          //! ListView를 child로 먹임과 동시에 내부 구조를 작성하게 된다.
+          padding: EdgeInsets.zero,
           children: [
-            Center(
-              child: CircleAvatar(
+            UserAccountsDrawerHeader(
+              //! 제일 위에 색칠한 탭. 자동이체닷컴에서는 이름, 계정 정보, 내정보관리 등이 들어갈 듯.
+              currentAccountPicture: CircleAvatar(
                 backgroundImage: AssetImage('assets/kimnakseo.jpeg'),
-                radius: 60.0,
+                backgroundColor: Colors.white,
+              ),
+              accountName: Text('NakSeo'),
+              accountEmail: Text('nakseono@gmail.com'),
+              onDetailsPressed: () {
+                print('arrow is clicked');
+              },
+              decoration: BoxDecoration(
+                color: Colors.red[200],
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(40),
+                    bottomRight: Radius.circular(40)),
               ),
             ),
-            Divider(
-              height: 60,
-              color: Colors.grey[850],
-              thickness: 0.5,
-              endIndent: 30,
-            ),
-            Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.black,
-                letterSpacing: 2.0,
+            ListTile(
+              //! ListView에서 탭 하나 하나를 의미함.
+              leading: Icon(
+                Icons.home,
+                color: Colors.grey[850],
               ),
+              title: Text('Home'),
+              onTap: () {
+                print('home button is clicked');
+              },
+              trailing: Icon(Icons.add),
             ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              '김낙서',
-              style: TextStyle(
-                  color: Colors.black,
-                  letterSpacing: 2.0,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              'LEVEL',
-              style: TextStyle(
-                color: Colors.black,
-                letterSpacing: 2.0,
+            ListTile(
+              //! ListView에서 탭 하나 하나를 의미함.
+              leading: Icon(
+                Icons.home,
+                color: Colors.grey[850],
               ),
+              title: Text('Home'),
+              onTap: () {
+                print('home button is clicked');
+              },
+              trailing: Icon(Icons.add),
             ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              '232',
-              style: TextStyle(
-                  color: Colors.black,
-                  letterSpacing: 2.0,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  '아크메이지(썬, 콜)',
-                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
-                )
-              ],
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  '유니온 7118',
-                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
-                )
-              ],
-            ),
-            Row(
-              children: [
-                Icon(Icons.check_circle_outline),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  '#메린이 #응애 #나 #아기서노',
-                  style: TextStyle(fontSize: 16.0, letterSpacing: 1.0),
-                )
-              ],
+            ListTile(
+              //! ListView에서 탭 하나 하나를 의미함.
+              leading: Icon(
+                Icons.home,
+                color: Colors.grey[850],
+              ),
+              title: Text('Home'),
+              onTap: () {
+                print('home button is clicked');
+              },
+              trailing: Icon(Icons.add),
             ),
           ],
         ),
